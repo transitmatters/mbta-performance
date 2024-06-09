3b21ac2b7388072a611811ff979798df97e973f3
Devin Matte <devinmatte@gmail.com>
2024-06-09 19:41:00 -0400

    Support LAMP based monthly csvs

--- mbta-performance/chalicelib/historic/backfill/main.py
+++ mbta-performance/chalicelib/historic/backfill/main.py
@@ -1,4 +1,4 @@
-from ..constants import ARCGIS_IDS
+from ..constants import ARCGIS_IDS, HISTORIC_COLUMNS_LAMP, HISTORIC_COLUMNS_PRE_LAMP
 from ..download import download_historic_data, list_files_in_dir, prep_local_dir, unzip_historic_data
 from ..process import process_events
 
@@ -11,7 +11,11 @@ def backfill_single_year(year: str):
     input_dir = unzip_historic_data(zip_file, f"data/input/{year}")
     # process the data
     for file in list_files_in_dir(input_dir):
-        process_events(file, "data/output")
+        # in 2024 data moved to LAMP and the format changed
+        if int(year) >= 2024:
+            process_events(file, "data/output", columns=HISTORIC_COLUMNS_LAMP)
+        else:
+            process_events(file, "data/output", columns=HISTORIC_COLUMNS_PRE_LAMP)
     print(f"Finished backfilling year {year}")
 
 
