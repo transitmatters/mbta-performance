98f607a08a532613c259738b34325de8e79bd251
Devin Matte <devinmatte@gmail.com>
2024-12-04 20:45:49 -0500

    Shift process timing for LAMP (#36)

--- mbta-performance/app.py
+++ mbta-performance/app.py
@@ -9,7 +9,7 @@ app = Chalice(app_name="mbta-performance")
 app.register_middleware(ConvertToMiddleware(datadog_lambda_wrapper))
 
 
-# Runs every 30 minutes from either 4 AM -> 1:55AM or 5 AM -> 2:55 AM depending on DST
-@app.schedule(Cron("*/30", "0-6,9-23", "*", "*", "?", "*"))
+# Runs every 30 minutes from either 5 AM -> 2:30AM or 6 AM -> 3:30 AM depending on DST
+@app.schedule(Cron("*/30", "0-7,10-23", "*", "*", "?", "*"))
 def process_daily_lamp(event):
     lamp.ingest_today_lamp_data()
