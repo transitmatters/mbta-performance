7360d965428718890234b1b086bb3d5601fa77ed
Andrew Kouré <50218177+ankoure@users.noreply.github.com>
2025-08-13 22:28:40 +0000

    Linted with Black (#44)

--- mbta-performance/chalicelib/historic/constants.py
+++ mbta-performance/chalicelib/historic/constants.py
@@ -23,7 +23,7 @@ HISTORIC_COLUMNS_PRE_LAMP = [
     "vehicle_label",
     "event_type",
     "event_time_sec",
-    "vehicle_consist"
+    "vehicle_consist",
 ]
 
 HISTORIC_COLUMNS_LAMP = [
@@ -37,5 +37,5 @@ HISTORIC_COLUMNS_LAMP = [
     "vehicle_label",
     "event_type",
     "event_time_sec",
-    "vehicle_consist"
+    "vehicle_consist",
 ]
